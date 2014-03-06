1ae1c2997e87ef14011c34ac8a50b0d415f36879
Mason Fabel <mason.fabel@gmail.com>
2014-03-06 13:02:09 -0800

    Created test file IO
    Created a basic file IO test that reads from one file into another
    one character at a time.
    
    main.c - Created file IO test.

--- main.c
+++ main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "utility.h"
 
 int main(int argc, char** argv) {
-	printf("Hello, world!\n");
+	char buff;
+	FILE* inf;
+	FILE* outf;
+
+	if(argc != 3) {
+		usage(argv[0]);
+		exit(0);
+	}
+
+	inf = fopen(argv[1], "r");
+	outf = fopen(argv[2], "w");
+
+	if(inf == NULL) {
+		error("unable to open input file.");
+	}
+
+	if(outf == NULL) {
+		error("unable to open output file.");
+	}
+
+	buff = fgetc(inf);
+	while(buff != EOF) {
+		fputc(buff, outf);
+		buff = fgetc(inf);
+	}
 
 	exit(0);
 }
